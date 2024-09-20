23d5acd90bb5ee9bd70b6616729f9b19ae6ac819
r0302772 <dennis.nijs@hotmail.com>
2024-09-20 21:21:40 +0200

    Material design

--- flutter/hello_flutter/lib/main.dart
+++ flutter/hello_flutter/lib/main.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const Center(
-      child: Text(
-        "Hello Flutter!",
-        textDirection: TextDirection.ltr,
+    const MaterialApp(
+      title: 'Hello Flutter App',
+      home: Material(
+        color: Colors.deepPurple,
+        child: Center(
+          child: Text(
+            "Hello Flutter!",
+            textDirection: TextDirection.ltr,
+            style: TextStyle(color: Colors.white, fontSize: 36.0),
+          ),
+        ),
       ),
     ),
   );
