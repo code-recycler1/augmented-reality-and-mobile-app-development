267f0d5248682fe8d8e6e6a3bf7ec079695f4aea
r0302772 <dennis.nijs@hotmail.com>
2024-09-20 21:23:54 +0200

    Scaffold class

--- flutter/hello_flutter/lib/main.dart
+++ flutter/hello_flutter/lib/main.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       title: 'Hello Flutter App',
-      home: Material(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Text(
-            "Hello Flutter!",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 36.0),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Title in App Bar"),
+        ),
+        body: const Material(
+          color: Colors.deepPurple,
+          child: Center(
+            child: Text(
+              "Hello Flutter!",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 36.0),
+            ),
           ),
         ),
       ),
