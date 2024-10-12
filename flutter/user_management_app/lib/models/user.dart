7d9bc51789e6c66296185a34f3dfc7fb6fda348e
r0302772 <dennis.nijs@hotmail.com>
2024-10-12 16:22:31 +0200

    start_api_tunnel & Create the User class

---
+++ flutter/user_management_app/lib/models/user.dart
@@ -0,0 +1,27 @@
+class User {
+  int id;
+  String email;
+  String firstname;
+  String lastname;
+
+  User(
+      {required this.id,
+      required this.email,
+      required this.firstname,
+      required this.lastname});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      firstname: json['first_name'],
+      lastname: json['last_name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'email': email,
+        'first_name': firstname,
+        'last_name': lastname,
+      };
+}
