44bf1f1536d13c7fdd19246cbdc2e000852003ac
r0302772 <dennis.nijs@hotmail.com>
2024-10-12 17:21:42 +0200

    Update the UserApi

--- flutter/user_management_app/lib/apis/user_api.dart
+++ flutter/user_management_app/lib/apis/user_api.dart
@@ -28,4 +28,49 @@ class UserApi {
       throw Exception('Failed to load user');
     }
   }
+
+  static Future<User> createUser(User user) async {
+    var url = Uri.https(server, '/users');
+
+    final http.Response response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(user),
+    );
+    if (response.statusCode == 201) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create user');
+    }
+  }
+
+  static Future<User> updateUser(int id, User user) async {
+    var url = Uri.https(server, '/users/$id');
+
+    final http.Response response = await http.put(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(user),
+    );
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to update user');
+    }
+  }
+
+  static Future deleteUser(int id) async {
+    var url = Uri.https(server, '/users/$id');
+
+    final http.Response response = await http.delete(url);
+    if (response.statusCode == 200) {
+      return;
+    } else {
+      throw Exception('Failed to delete user');
+    }
+  }
 }
