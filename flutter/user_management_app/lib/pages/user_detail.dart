816ac69944edc31c2fa4d0989c9ba3034733a73a
r0302772 <dennis.nijs@hotmail.com>
2024-10-12 17:18:46 +0200

    Create a detail page

---
+++ flutter/user_management_app/lib/pages/user_detail.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import '../models/user.dart';
+import '../apis/user_api.dart';
+
+class UserDetailPage extends StatefulWidget {
+  final int id;
+
+  const UserDetailPage({Key? key, required this.id}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _UserDetailPageState();
+}
+
+class _UserDetailPageState extends State<UserDetailPage> {
+  User? user;
+
+  TextEditingController firstnameController = TextEditingController();
+  TextEditingController lastnameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getUser(widget.id);
+  }
+
+  void _getUser(int id) {
+    UserApi.fetchUser(id).then((result) {
+      setState(() {
+        user = result;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User details"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5.0),
+        child: _userDetails(),
+      ),
+    );
+  }
+
+  _userDetails() {
+    if (user == null) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      TextStyle? textStyle = Theme.of(context).textTheme.bodyText1;
+
+      firstnameController.text = user!.firstname;
+      lastnameController.text = user!.lastname;
+      emailController.text = user!.email;
+
+      return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: firstnameController,
+              style: textStyle,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "First Name",
+                labelStyle: textStyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+            ),
+            Container(
+              height: 15,
+            ),
+            TextField(
+              controller: lastnameController,
+              style: textStyle,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Last Name",
+                labelStyle: textStyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+            ),
+            Container(
+              height: 15,
+            ),
+            TextField(
+              controller: emailController,
+              style: textStyle,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "Email",
+                labelStyle: textStyle,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+}
